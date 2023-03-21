1ac9839291babbb5b2123f1401b316c6f1110498
sourav88 <127320821+sourav88@users.noreply.github.com>
2023-03-22 03:13:53 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,11 @@ def newBuild()
 {
   sh 'mvn package'
 }
+def newDeploy(jobname,ip,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def newTest(jobname1)
+{
+  sh " java -jar /var/lib/jenkins/workspace/${jobname1}/testing.jar"
+}
